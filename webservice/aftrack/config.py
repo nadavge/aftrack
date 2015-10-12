6f5bb94f0a1f30eeeb13cc4b2ee30cf1fb487889
Nadav Geva <nadav.geva4@gmail.com>
2015-10-12 23:54:20 +0300

    Fix secret key file bugs

--- webservice/aftrack/config.py
+++ webservice/aftrack/config.py
@@ -11,14 +11,35 @@ def create_key_file():
 	""" Creates a key file for session handling.
 	The key file is stored in SECRET_KEY_FILE, and constitues of
 	SECRET_KEY_LEN bytes """
-	
+
+	secret_key = os.urandom(SECRET_KEY_LEN)
+
 	try:
 		with open(SECRET_KEY_FILE, 'wb') as secret_key_file:
-			secret_key_file.write(os.urandom(SECRET_KEY_LEN))
+			secret_key_file.write(secret_key)
 			print('\'%s\' created successfully!'%SECRET_KEY_FILE)
+			
 	except IOError:
 		print('Unable to create \'%s\', exiting!'%SECRET_KEY_FILE)
 		sys.exit(1)
+
+	return secret_key
+
+
+def read_key_file():
+	""" Read the secret key from the relevant file """
+	try:
+		with open(SECRET_KEY_FILE, 'rb') as secret_key_file:
+			secret_key = secret_key_file.read()
+	
+		if len(secret_key) != SECRET_KEY_LEN:
+			print("Secret key in file is not of valid length")
+
+	except IOError:
+		print('No \'%s\' file, creating one now...'%SECRET_KEY_FILE)
+		secret_key = create_key_file()
+
+	return secret_key
 
 
 class Config(object):
@@ -28,14 +49,7 @@ class Config(object):
 	MAX_COOKIE_AGE = timedelta(days=30).total_seconds()
 	# TODO remove after deployment in case of other database
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASEDIR, 'aftrack.db'))  
-	 
-	def __init__(self):
-		try:
-			with open(SECRET_KEY_FILE, 'rb') as secret_key_file:
-				self.SECRET_KEY = secret_key_file.read()
-		except IOError:
-			print('No \'%s\' file, creating one now...'%SECRET_KEY_FILE)
-			create_key_file()
+	SECRET_KEY = read_key_file()
 
 
 class ProductionConfig(Config):
