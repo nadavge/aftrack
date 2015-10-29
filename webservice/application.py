b991f59e375e5168e0835a99355efa1ae19eab37
Nadav Geva <nadavgeva4@gmail.com>
2015-10-29 19:59:00 +0200

    Add ssl option in comments

--- webservice/application.py
+++ webservice/application.py
@@ -1,5 +1,9 @@
 from aftrack import app as application
+#import ssl
 
+#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+#context.load_cert_chain('aftrack.crt', 'aftrack.key')
 if __name__=="__main__":
+	#application.run(host='0.0.0.0',ssl_context=context)
 	application.run()
 
